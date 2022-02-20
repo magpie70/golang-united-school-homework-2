87b1165d70d447ead7d234379eac0e2c5ee2d01a
magpie70 <adlet.anarbek@alumni.nu.edu.kz>
2022-02-20 15:41:02 +0600

    HW2

--- solution.go
+++ solution.go
@@ -9,13 +9,16 @@ import "math"
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
+var SidesTriangle uint8 = 3
+var SidesSquare uint8 = 4
+var SidesCircle uint8 = 0
 
 func CalcSquare(sideLen float64, sidesNum uint8) float64 {
-	if sidesNum == 0{
+	if sidesNum == SidesCircle{
 		return math.Pi*sideLen*sideLen
-	} else if sidesNum == 3{
+	} else if sidesNum == SidesTriangle{
 		return math.Sqrt(3)/4*sideLen*sideLen
-	} else if sidesNum == 4{
+	} else if sidesNum == SidesSquare{
 		return sideLen*sideLen
 	} else{
 		return 0
